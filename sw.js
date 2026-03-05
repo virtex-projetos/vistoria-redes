7915a0b1945a77a68deece2f76db63d586daecc7
daniellavor-dotcom <daniel.lavor@virtex.com.br>
2026-03-05 15:39:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,29 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Instalado');
+const CACHE_NAME = 'virtex-vistoria-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './virtex3.e7d19001.png'
+];
+
+// Instala o motor e guarda os arquivos na memória do celular
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Salvando arquivos offline');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Isso atende a exigência do Google Chrome para PWA
+// Intercepta a internet: se estiver offline, puxa da memória!
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retorna o arquivo salvo ou vai na internet buscar
+        return response || fetch(event.request);
+      })
+  );
 });
